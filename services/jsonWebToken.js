b7a42af0715111443deac93cf57d6fee5832ba6a
namos <lobamo@salocina.com>
2022-05-07 19:03:43 +0200

    verify token

--- services/jsonWebToken.js
+++ services/jsonWebToken.js
@@ -7,11 +7,22 @@ class Jwt {
     sign_in(username, password)
     {
         const private_key = fs.readFileSync('keys/jwtRS256.key', {encoding: 'utf-8'})
-        console.log(private_key)
         const token = jwt.sign({username: username, password: password}, private_key, {algorithm: 'RS256'})
         return token
+    }
+
+    verify_token(token)
+    {
+        const public_key = fs.readFileSync('keys/jwtRS256.key.pub', {encoding: 'utf-8'})
+        const payload = jwt.verify(token, public_key, {algorithms: 'RS256'});
+        return payload;
+
     }
 }
 
 const jwt_test = new Jwt();
-jwt_test.sign_in('nicolas', 'test');
+const token = jwt_test.sign_in('nicolas', 'test');
+
+const payload = jwt_test.verify_token(token);
+
+console.log(payload);
