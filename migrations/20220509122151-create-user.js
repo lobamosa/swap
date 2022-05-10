34667501ebed09eafbd7524573b2643840e853e6
namosdjoro <namosdjoro@emmaus.asso.fr>
2022-05-10 17:21:25 +0200

    add wishlist

--- migrations/20220509122151-create-user.js
+++ migrations/20220509122151-create-user.js
@@ -25,6 +25,9 @@ module.exports = {
       library: {
         type: Sequelize.ARRAY(Sequelize.STRING)
       },
+      whishlist: {
+        type: Sequelize.ARRAY(Sequelize.STRING)
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
