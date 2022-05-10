3ab9be1bf29962b3de3d0fd45c8777804f7e304b
namosdjoro <namosdjoro@emmaus.asso.fr>
2022-05-10 11:54:15 +0200

    test

--- tests/testAuth.js
+++ tests/testAuth.js
@@ -1,15 +1,29 @@
 var assert = require('assert');
+const bcrypt = require('bcrypt');
 const { expect } = require('chai');
 const { Sequelize, User } = require('../models/');
+var SequelizeMock = require('sequelize-mock');
+
 const Auth = require('../services/auth');
 describe('Authentication module', async function () {
     it('should create an user', async function () {
       const Auth_class = new Auth
-      const username = "johndoe";
-      const email = "jdoe@gm.com";
+      const user =  new User;
+      user.username = "johndoe";
+      user.email = "jdoe@gm.com";
+      user.password = "aPassToPassThePass";
+      user.library = ["mgs5", "ff7"];
+      const user_unstance =  await Auth_class.create_user(user)
+      expect(user_instance.username).to.equal(username);
+    });
+    it('expect password to be encrypted',async () => {
+      const Auth_class = new Auth
+      const username = "johndo";
+      const email = "je@gm.com";
       const password = "aPassToPassThePass";
       const library = ["mgs5", "ff7"];
-      const user =  await Auth_class.create_user(username, email, password, library)
-      expect(user.username).to.equal(username);
-    });
+      const hash =  await Auth_class.hash_password(password, 10)
+      const password_to_check = bcrypt.compareSync(password, hash)
+      expect(password_to_check).to.be.true
+    })
 });
--- tests/testLibrary.js
+++ tests/testLibrary.js
@@ -2,6 +2,7 @@ var assert = require('assert');
 const { expect } = require('chai');
 const { password } = require('pg/lib/defaults');
 const {User} = require('../models/');
+const Auth = require('../services/auth');
 const Library = require('../services/library');
 
 describe('Library service', () => {
@@ -11,10 +12,11 @@ describe('Library service', () => {
         const email = "jdoe@gmail.com";
         const library = ['ff7', 'mgs5'];
         const service = new Library;
-        const library_from_user = await service.get_games_from_user('jdoe');
-        for(let i = 0; i < library_from_user.length; i++)
-        {
-            console.log(library_from_user[i]);
-        }
+        const Auth_class = new Auth;
+        const user = new User
+        user.username = username;
+        user.library = library;
+        const library_from_user = await service.get_games_from_user(user);
+        expect(library_from_user).to.be.an('array')
     })
 }) 
