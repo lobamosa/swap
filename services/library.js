d690f0f392c89240b2fa794471aa6e7ccc7ebfa9
namosdjoro <namosdjoro@emmaus.asso.fr>
2022-05-10 14:49:45 +0200

    Library class with get whishlist and retrieve user game

--- services/library.js
+++ services/library.js
@@ -1,14 +1,24 @@
 const User = require('../models/').User
-
+const binarySearch = require('./ds')
 class Library {
     constructor()
     {
         
     }
-    async get_games_from_user(user)
+    async get_library_from_user(user)
     {
         const library = user.library;
         return library; 
+    }
+    async get_whishlist_from_user(user)
+    {
+        const whishlist = user.whishlist;
+        return whishlist; 
+    }
+    async retrieve_user_game(whishlist, game)
+    {
+        const game_wanted = binarySearch(whishlist, game);
+        return game_wanted;
     }
 }
 
