3830915ec0839af1ef2b7e33590462f21868a0ea
nicolas AMOS <namosdjoro@emmaus.asso.fr>
2022-05-11 15:42:09 +0200

    sign up page

--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,31 @@ var bodyParser = require('body-parser');
 const Auth = require('../services/auth');
 const Jwt = require('../services/jwt');
 const { AccessDeniedError } = require('sequelize/types');
+const user = require('../models/user');
+const cookieParser = require('cookie-parser');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+router.get('/signup', (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const username = req.body.username;
+  const library = req.body.library;
+  const whishlist = req.body.whishlist;
+
+  const authentication = new Auth;
+  const user = authentication.create_user(username, email, password, library, whishlist)
+  try {
+    res.send({message: "user created"});
+  }
+  catch(error)
+  {
+    res.send({error: error});
+  }
+})
+
 router.get('/authenticate', (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password
