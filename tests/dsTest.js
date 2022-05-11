47f29486e8980c1a2bdca82e3559bc4a12c7447b
nicolas AMOS <namosdjoro@emmaus.asso.fr>
2022-05-11 13:51:20 +0200

    test binary search

---
+++ tests/dsTest.js
@@ -0,0 +1,14 @@
+var assert = require('assert');
+const bcrypt = require('bcrypt');
+const { expect } = require('chai');
+const lib = require('pg-hstore');
+const binarySearch = require('../services/ds');
+
+describe('data structure', () => {
+    it('should retrieve the value in n(log n) with binary searcg', () => {
+        const library = ['ff7', 'metal gear solid 5', 'metal gear solid 3', 'paper mario', 'zelda ocarina of time'];
+        const target = 'paper mario'
+        const retrieved_game =  binarySearch(library, target);
+        expect(retrieved_game).to.equal(target);
+    })
+})
