9deef3f3f05474b0de1cedf37a35d8a49f17f3af
nicolas AMOS <namosdjoro@emmaus.asso.fr>
2022-05-11 15:30:57 +0200

    remove migrations folder

--- sequelize.js
+++ sequelize.js
@@ -20,7 +20,6 @@ const sequelize = new Sequelize("swap", 'admin', 'admin', sequelizeConfig);
 
 sequelize.authenticate().then(() => {
     require("./migration")
-    require("./migrations")
     console.log('connection ok')
   })
 module.exports = sequelize
