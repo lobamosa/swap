94401e8dbb23bb2047401d9e073cad5e2778352e
nicolas AMOS <namosdjoro@emmaus.asso.fr>
2022-05-11 15:48:06 +0200

    change the descrption to test bs

--- tests/dsTest.js
+++ tests/dsTest.js
@@ -2,7 +2,7 @@ const { expect } = require('chai');
 const binarySearch = require('../services/ds');
 
 describe('data structure', () => {
-    it('should retrieve the value in n(log n) with binary searcg', () => {
+    it('should retrieve the value with a binary search', () => {
         const library = ['ff7', 'metal gear solid 5', 'metal gear solid 3', 'paper mario', 'zelda ocarina of time'];
         const target = 'paper mario'
         const retrieved_game =  binarySearch(library, target);
